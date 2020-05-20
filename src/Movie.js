c1cb044d89ff6f15d987b5d4fef792af05319e77
kjaa34@gmail.com <kjaa34@gmail.com>
2020-05-21 02:32:32 +0900

    Component -> Function(stateless)

--- src/Movie.js
+++ src/Movie.js
@@ -1,33 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import PropTypes from "prop-types";
 import './Movie.css';
 
 
-class Movie extends Component{
-
-    /*
-    static propTypes = {
-        title: PropTypes.string,
-        poster: PropTypes.string
-    }
-    */
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}></MoviePoster>
-                <h1>{this.props.title}</h1>
-            </div>            
-        );        
-    }
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster}></MoviePoster>
+            <h1>{title}</h1>
+        </div>            
+    )
 }
 
-class MoviePoster extends Component{
-    render(){
-        return(
-            <img src={this.props.poster} width="300"></img>
-        );
-    }
+function MoviePoster({poster}){
+    return (
+        <img src={poster} width="300" alt="Movie Poster"></img>
+    )
 }
 
 export default Movie;
