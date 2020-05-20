d4b8a1de9ef62a7179ca0ec3e43c90d282d9d756
kjaa34@gmail.com <kjaa34@gmail.com>
2020-05-21 02:47:12 +0900

    Fetch api movie lists.

--- src/App.js
+++ src/App.js
@@ -6,36 +6,10 @@ import Movie from './Movie';
 
 class App extends Component {
 
-  state = {
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title:"Matris",
-            poster:"https://cdn.pixabay.com/photo/2016/11/21/22/08/white-male-1847732_960_720.jpg"
-          },
-          {
-            title:"Full Matel Jacket",
-            poster:"https://cdn.pixabay.com/photo/2017/06/02/22/01/dog-2367414_960_720.png"
-          },
-          {
-            title:"Star wars",
-            poster:"https://cdn.pixabay.com/photo/2015/10/31/12/00/cinema-1015320_960_720.jpg"
-          },
-          {
-            title:"Old Boy",
-            poster:"https://cdn.pixabay.com/photo/2015/12/05/21/45/filmklappe-1078813_960_720.png"
-          },
-          {
-            title:"Trainspotting",
-            poster:"https://cdn.pixabay.com/photo/2015/12/05/21/45/filmklappe-1078813_960_720.png"
-          }
-        ]
-      })
-    }, 5000);
+    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
   }
 
   _renderMovies = () => {
