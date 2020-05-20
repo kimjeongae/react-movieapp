7f4b0e6a0672bfca2a45ef9dd8bb93194e09d840
kjaa34@gmail.com <kjaa34@gmail.com>
2020-05-21 05:27:16 +0900

    App.js edited.

--- src/App.js
+++ src/App.js
@@ -38,9 +38,10 @@ class App extends Component {
   }
 
   render(){
-    return (
-      <div className="App">              
-        {this.state.movies ? this._renderMovies() : 'Loading'}
+    const { movies } = this.state;
+    return (      
+      <div className={movies ? "App" : "App--loading"}>              
+        { movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }  
