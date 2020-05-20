5b142660d5865db383f819dd3639113edc8d2151
kjaa34@gmail.com <kjaa34@gmail.com>
2020-05-21 03:24:07 +0900

    Movie lists get&rendered

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ class App extends Component {
   state = {}
 
   componentDidMount(){
-    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map( (movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={movie.index}></Movie>
+    const movies = this.state.movies.map( (movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}></Movie>
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    .then(result => result.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render(){
